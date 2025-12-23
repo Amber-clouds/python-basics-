ad0a49dff5dd0c8997f4ae6662dc3d2e1de5396a
Amber-clouds <amberashraf421@gmail.com>
2025-12-23 23:00:13 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,16 @@
+loops.py
+
+# type: for loop
+# This program uses a for loop to print numbers from 1 to 5.
+
+for number in range(1, 6):
+    print(number)
+
+# Type: While loop
+# This program uses a while loop to print numbers from 1 to 5.
+
+count = 1
+
+while count <= 5:
+    print("While loop count:", count)
+    count += 1
