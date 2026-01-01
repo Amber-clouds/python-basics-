8e0b02a58540bb42a51bcc72b9917052ff013a47
Amber-clouds <amberashraf421@gmail.com>
2026-01-01 23:16:25 +0500

    extended Variables.py with a new practice profram

--- Variables.py
+++ Variables.py
@@ -5,3 +5,12 @@ age = 15
 
 print("My name is", name)
 print("I am", age, "years old")
+
+#this program inputs your age
+birth_year = input("Enter your birth year: ")
+birth_year= int(birth_year)
+
+current_year= 2026
+age = current_year - birth_year
+
+print("you are",age,"years old")
